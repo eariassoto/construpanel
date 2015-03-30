22bcafa75a43dc8703e0b9a628cf89af6ec3bd13
eariassoto <ariassotoemmanuel@gmail.com>
2015-03-30 09:28:12 -0600

    colores listo

--- productos/myApp.js
+++ productos/myApp.js
@@ -2,10 +2,17 @@
     var app = angular.module("myApp", []);
 
     app.controller("productsCtrl", ['$scope', '$http', function($scope, $http) {
+
+        $http.get('./colores.json').success(function(data){
+            $scope.colores = data;
+        });
         $http.get('./productos.json').success(function(data){
-            console.log("got it", data);
             $scope.productos = data;
         });
+
+        $scope.color = function(i){
+            return {'background-color': i.color};
+        }
     }]);
 })();
 
