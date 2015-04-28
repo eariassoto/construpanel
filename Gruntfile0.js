6aac5521023d632e71eab36f5e5849b17ec9eb0a
eariassoto <ariassotoemmanuel@gmail.com>
2015-04-28 13:04:01 -0600

    antes salir

--- Gruntfile0.js
+++ Gruntfile0.js
@@ -1,97 +1,67 @@
-module.exports = function (grunt) {
+/*	JFlex example: Recongnizing numbers (integers, octals, hexs) and identificators
+	Luis Alberto Pérez García @UEM
+	luixal@gmail.com
+*/
 
-    grunt.initConfig({
-        clean: {
-            build: {
-                src: ['build']
-            }
-        },
-        useminPrepare: {
-            html: "source/index.html",
-            options: {
-                dest: "build/"
-            }
-        },
-        usemin: {
-            html: ["build/index.html"]
-        },
-        copy: {
-            build: {
-                src: "source/index.html",
-                dest: "build/index.html"
-            },
-            dist: {
-                files: [
-                    {
-                        cwd: 'source/assets',
-                        src: ['**'],
-                        dest: 'build/assets',
-                        expand: true
-                    },
-                    {
-                        cwd: 'source/bower_components/font-awesome',
-                        src: ['fonts/*'],
-                        dest: 'build/',
-                        expand: true
-                    },
-					{
-                        cwd: 'source/bower_components/bootstrap',
-                        src: ['fonts/*'],
-                        dest: 'build/',
-                        expand: true
-                    },
-                    {
-                        cwd: 'source/bower_components/open-sans-fontface',
-                        src: ['fonts/**/*'],
-                        dest: 'build/css/',
-                        expand: true
-                    }
-                ]
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 4000,
-                    base: 'build',
-                    hostname: '*'
-                }
-            }
-        },
-        watch: {
-            copy: {
-                files: ['source/**', '!source/**/*.css', '!source/**/*.js'],
-                tasks: ['copy']
-            }
-        }
-    });
+/* USERCODE Area: Here go the imports I need (no package needed for this example :P): */
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-usemin');
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    grunt.registerTask(
-        'build',
-        'Compiles all of the assets and copies the files to the build directory.',
-        [
-            'clean',
-            'copy',
-            'useminPrepare',
-            'concat',
-            'cssmin',
-            'uglify',
-            'usemin'
-        ]
-    );
+%%
 
-    grunt.registerTask(
-        'serve',
-        'Watches the project for changes, automatically builds them and runs a server.',
-        ['connect', 'watch']
-    );
-};
+/* OPTIONS, DECLARATIONS AND MACROS Area: Here there's more stuff, inline commented: */
+
+%class MyLexerExample   /* this sets the name I want to use to my class. Files will be named MyLexerExample.java and MyLexerExample.class once it's "compiled" */
+%unicode                /* Maybe it's not necessary, but ey! any developer should work with text files in unicode format! :P */
+%line                   /* switches on the line counter (stored in variable "yyline") */
+%column                 /* switches on the column counter (stored in variable "yycolumn") */
+%standalone             /* adds a main method that will expect an input file passed as an argument and will show any information about tokens to the standard output */
+%type String            /* sets the type returned by yylex() function /*
+
+/* Here goes an important part: Java Code. We include here the variables and methods we want to use, all this code will be copied literally in the resultant code. */
+
+%init{ /* The %init directive allows us to introduce code in the class constructor. We are using it for initializing our variables */
+this.tokensList = new ArrayList();
+%init}
+
+%{
+
+private ArrayList tokensList; /* our variable for storing token's info that will be the output */
+
+private void writeOutputFile() throws IOException { /* our method for writing the output file */
+	String filename = "file.out";
+	BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+	for (String s:this.tokensList) {
+		System.out.println(s);
+		out.write(s + "\n");
+	}
+	out.close();
+}
+
+%}
+
+/* Here goes another importar part: Macros. We're using this to include the regular expressions that recognize out tokens.
+Basically, macros are used to name regular expressions so actions below  */
+
+Decimal     = [0-9]+                /* Macro for Integer numbers */
+Octal       = "o"[0-7]+             /* Macro for Octal numbers */
+Hex         = "0x"[0-9|A-F]+        /* Macro for Hexadecimal numbers */
+Identifier  = [a-zA-Z][a-zA-Z0-9_]* /* Macro for Identifiers */
+
+/* Here you can also include states, but as I don't know yet what use can I give them I'm not including them */
+
+%%
+
+/* LEXICAL RULES AND ACTIONS Area: Here we introduce the code to be executed when input matches a regular expression.
+As we have named them using the macros we use those names here. */
+
+{Decimal}       {this.tokensList.add("[" + yyline + "," + yycolumn + "] Decimal: " + yytext());}     /* Code executed when an Integer is found */
+{Octal}         {this.tokensList.add("[" + yyline + "," + yycolumn + "] Octal: " + yytext());}       /* Code executed when an Octal is found */
+{Hex}           {this.tokensList.add("[" + yyline + "," + yycolumn + "] Hexadecimal: " + yytext());} /* Code executed when an Hexadecimal is found */
+{Identifier}    {this.tokensList.add("[" + yyline + "," + yycolumn + "] Identifier: " + yytext());}  /* Code executed when an Identifier is found */
+\r|\n|\r\n|\u2028|\u2029|\u000B|\u000C|\u0085 {}           /* Lexical rule for avoiding crashing when and EOL is found, actually, we do nothing here */
+<< EOF >>       {this.writeOutputFile(); System.exit(0);}  /* Code executen when reached the EOF */
+.               {}                                         /* Lexical tule for avoinding crashing when something strange is found, we do nothing here too */
